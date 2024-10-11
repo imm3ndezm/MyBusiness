fb37cd8ee90bb7ede760d098af9b1557c4e33c79
Alejandro Mendez <marencomendezaa@gmail.com>
2024-10-10 23:05:09 -0600

    First commit: Uploading proyect to GitHub.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,8 +57,17 @@
 
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
-        </div>
 
+        </div>
+        <p class="flex items-start mt-2 text-xs text-slate-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mr-1.5">
+                <path fill-rule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                    clip-rule="evenodd" />
+            </svg>
+
+            Use at least 8 characters, one uppercase, one lowercase and one number.
+        </p>
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 href="{{ route('login') }}">
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
